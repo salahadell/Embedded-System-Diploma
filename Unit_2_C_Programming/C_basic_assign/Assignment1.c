a820278dcd1a05f96918baf7cff5030d368f1f52
Salah Adel <salah-adel2011@hotmail.com>
2023-03-23 21:42:45 +0200

    added first Ex/Assignment1

---
+++ Unit_2_C_Programming/C_basic_assign/Assignment1.c
@@ -0,0 +1,24 @@
+/*
+ ============================================================================
+ Name        : test1.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ for(int i =0;i<10;i++)
+ puts("!!!Hello World!!!");
+ return EXIT_SUCCESS;
+ */
+
+int main()
+{
+	printf("C Programming");
+	return 0;
+
+}

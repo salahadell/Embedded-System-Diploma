e393d0c6e33507cbf5f1f715174eb12792ecb0f3
Salah Adel <salah-adel2011@hotmail.com>
2023-03-23 22:23:39 +0200

    added EX2&Ex3

--- Unit_2_C_Programming/C_basic_assign/Assignment1.c
+++ Unit_2_C_Programming/C_basic_assign/Assignment1.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(void)
 {
-	printf("C Programming");
+	/*Ex1
+	printf("C Programming");*/
+	/*Ex2
+	int a;
+	printf("Enter an integer: ");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%d",&a);
+	printf("you entered: %d",a);*/
+	int a;
+	int b;
+	float sum;
+	printf("enter two numbers: ");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%d %d",&a,&b);
+	sum=a+b;
+	printf("sum: %f ",sum);
 	return 0;
 
 }

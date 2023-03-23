e52568de267fcf63c704dc2ba65d7c1cd09bd099
Salah Adel <salah-adel2011@hotmail.com>
2023-03-23 21:47:21 +0200

    delete comments

--- Unit_2_C_Programming/C_basic_assign/Assignment1.c
+++ Unit_2_C_Programming/C_basic_assign/Assignment1.c
@@ -1,20 +1,5 @@
-/*
- ============================================================================
- Name        : test1.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
-/*
- for(int i =0;i<10;i++)
- puts("!!!Hello World!!!");
- return EXIT_SUCCESS;
- */
 
 int main()
 {

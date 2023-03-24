02f711c2afc5c0f2d64dea717542112370ea0620
Salah Adel <salah-adel2011@hotmail.com>
2023-03-24 04:53:14 +0200

    added Ex3

--- Unit_2_C_Programming/C_basic_assign/Assignment2.c
+++ Unit_2_C_Programming/C_basic_assign/Assignment2.c
@@ -19,7 +19,7 @@ int main()
 	else
 		printf("%d is odd",a);*/
 
-    /*Ex2
+	/*Ex2
 	char choice;
 	printf("Enter an alphabet: ");
 	fflush(stdin);fflush(stdout);
@@ -44,7 +44,25 @@ int main()
 	    printf("%c is a consonant",choice);
 	    break;
 	}*/
-
+	/*Ex3
+	float a,b,c;
+	printf("enter three numbers: ");
+	fflush(stdout);fflush(stdin);
+	scanf("%f %f %f",&a,&b,&c) ;
+	if(a>=b)
+	{
+		if(a>=c)
+			printf("largest number= %.2f ",a);
+		else
+			printf("largest number= %.2f ",c);
+	}
+	else
+	{
+		if(b>=c)
+			printf("largest number= %.2f ",b);
+		else
+			printf("largest number= %.2f ",c);
+	}*/
 	return 0;
 }
 

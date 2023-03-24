66ec599b5f6bfa4035c8241eefb6cb315e9165a0
Salah Adel <salah-adel2011@hotmail.com>
2023-03-24 02:32:17 +0200

    added Ex2/Assignment2

--- Unit_2_C_Programming/C_basic_assign/Assignment2.c
+++ Unit_2_C_Programming/C_basic_assign/Assignment2.c
@@ -9,6 +9,7 @@
 
 int main()
 {
+	/*Ex1
 	int a;
 	printf("Enter an integer you want to check: ");
 	fflush(stdin);fflush(stdout);
@@ -16,9 +17,34 @@ int main()
 	if(!(a%2))
 		printf("%d is even",a);
 	else
-		printf("%d is odd",a);
+		printf("%d is odd",a);*/
 
+    /*Ex2
+	char choice;
+	printf("Enter an alphabet: ");
+	fflush(stdin);fflush(stdout);
+	scanf("%c",&choice);
+	switch (choice)
+	{
+	case 'a':
+	case 'A':
+	case 'o':
+	case 'O':
+	case 'u':
+	case 'U':
+	case 'i':
+	case 'I':
+	case 'e':
+	case 'E':
+	{
+		printf("%c is a vowel",choice);
+	    break;
+	}
+	default:
+	    printf("%c is a consonant",choice);
+	    break;
+	}*/
 
-   return 0;
+	return 0;
 }
 

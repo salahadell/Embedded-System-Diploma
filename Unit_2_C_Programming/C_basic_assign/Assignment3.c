67e5347a15227c05f8801682a5d7fabe33585bff
Salah Adel <salah-adel2011@hotmail.com>
2023-03-26 20:17:42 +0200

    added Ex7

--- Unit_2_C_Programming/C_basic_assign/Assignment3.c
+++ Unit_2_C_Programming/C_basic_assign/Assignment3.c
@@ -159,6 +159,20 @@ int main()
 			freq+=1;
 	}
 	printf("Frequency of %c = %d ",searched_char,freq);*/
+
+	/*Ex7 (find the length of a string)
+	char string [100];
+	int length=0;
+	printf("Enter a string: ");
+	fflush(stdin);fflush(stdout);
+	gets(string);
+
+	for(int i=0;string[i]!=0;i++)
+	{
+			length+=1;
+	}
+	printf("Length of string: %d ",length);*/
+
 	return 0;
 
 }

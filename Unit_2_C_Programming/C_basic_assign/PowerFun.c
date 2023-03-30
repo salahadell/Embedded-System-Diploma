64b919b18424af4a7ac2f90d08da397758c3134b
Salah Adel <salah-adel2011@hotmail.com>
2023-03-30 19:14:44 +0200

    delete comments

--- Unit_2_C_Programming/C_basic_assign/PowerFun.c
+++ Unit_2_C_Programming/C_basic_assign/PowerFun.c
@@ -1,13 +1,3 @@
-/*
- ============================================================================
- Name        : PowerFun.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 int PowerFun(int base, int power);
